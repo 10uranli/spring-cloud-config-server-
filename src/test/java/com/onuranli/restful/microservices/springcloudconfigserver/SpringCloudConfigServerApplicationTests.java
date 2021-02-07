33f7f0687dd2c08ee87c3a31d8f5aa2e0ca8a73b
10uranli <onur.anli@bil.omu.edu.tr>
2021-02-07 15:26:09 +0300

    initiliaze project

---
+++ src/test/java/com/onuranli/restful/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onuranli.restful.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
